a2463e813b70588a54da21745abfa25467051690
kepawni <a+kepawni@commodea.com>
2018-12-20 00:27:25 +0100

    Add trait using windUp() for equals()

---
+++ src/Basic/EqualsUsesWindUp.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+namespace Kepawni\Twilted\Basic;
+
+trait EqualsUsesWindUp
+{
+    /**
+     * @param mixed $other Another value to test.
+     *
+     * @return bool True when the other value is equal to this instance.
+     */
+    public function equals($other): bool
+    {
+        return $other instanceof static && $this->windUp() === $other->windUp();
+    }
+}
